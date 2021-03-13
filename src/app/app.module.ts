1ff654e21813e7ceea74c80d1aacaee2c2eb5626
Abdelrahman Tealab <62073015+AbdelrahmanTealab@users.noreply.github.com>
2021-03-12 19:27:35 -0500

    integrate application with backend api server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import {HttpClientModule} from "@angular/common/http";
+import {HTTP} from "@ionic-native/http/ngx";
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [HttpClientModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },HTTP],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/mainscreen/mainscreen.page.ts
+++ src/app/mainscreen/mainscreen.page.ts
@@ -1,15 +1,67 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { HTTP } from '@ionic-native/http/ngx';
+import { LoadingController, Platform } from '@ionic/angular';
+import { from } from 'rxjs';
+import {finalize} from "rxjs/operators";
 @Component({
   selector: 'app-mainscreen',
   templateUrl: './mainscreen.page.html',
   styleUrls: ['./mainscreen.page.scss'],
 })
 export class MainscreenPage implements OnInit {
+  constructor(private http: HttpClient, private nativeHttp: HTTP, private plt: Platform, private loadingCtrl: LoadingController) {
 
-  constructor() { }
+  }
+
+  data = [];
+
+  async getAllData(){
+    let loading = await this.loadingCtrl.create();
+    await loading.present();
+
+    this.http.get('https://sem1-project-nodejs.herokuapp.com/patients').pipe(
+      finalize(() => loading.dismiss())
+    )
+    .subscribe(data => {
+      this.data = data['results'];
+    },err => {console.log('error during js call: ',err);});
+  }
+
+
+  async getDataNative(){
+    let loading = await this.loadingCtrl.create();
+    await loading.present();
+
+    let nativeCall = this.nativeHttp.get('https://sem1-project-nodejs.herokuapp.com/patients',{},{
+      'Content-Type':'application/json'
+    });
+    from(nativeCall).pipe(
+      finalize(() => loading.dismiss())
+    )
+    .subscribe(data => {
+      console.log("NAtive data: ",data);
+      let parsed = JSON.parse(data.data);
+      console.log(" my parsed data: ", parsed);
+      this.data = parsed;
+      console.log(" my final data: ", this.data);
+
+
+
+    },err => {console.log('error during js call: ',err);});
+  }
+
+
+  getData(){
+    if (this.plt.is('cordova')){
+      this.getDataNative();
+    } else {
+      this.getAllData();
+    }
+  }
 
   ngOnInit() {
-  }
+
+    }
 
 }
